fba0e932feb8460184e3af1b775a28f69e36074b
georgeBl <blanarugeorge96@gmail.com>
2025-02-06 15:27:29 +0000

    Added a car repo with service

---
+++ Tutorial.Domain/CarRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Domain
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllAsync();
+        Task<Car> GetByIdAsync(int id);
+        Task AddAsync(Car car);
+        Task UpdateAsync(Car car);
+        Task DeleteAsync(int id);
+    }
+
+    public class CarRepository : ICarRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CarRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Car>> GetAllAsync()
+        {
+            return await _context.Cars.ToListAsync();
+        }
+
+        public async Task<Car> GetByIdAsync(int id)
+        {
+            return await _context.Cars.FindAsync(id);
+        }
+
+        public async Task AddAsync(Car car)
+        {
+            await _context.Cars.AddAsync(car);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Car car)
+        {
+            _context.Cars.Update(car);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Tutorial.Domain/CarService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Domain
+{
+    public class CarService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async Task CreateCar()
+        {
+            var car = new Car
+            {
+                Manufacturer = "Tesla",
+                Model = "Model 3",
+                Registration = "XYZ123",
+                Year = 2023
+            };
+
+            await _carRepository.AddAsync(car);
+        }
+        public async Task GetAllCars()
+        {
+            var cars = await _carRepository.GetAllAsync();
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"Id: {car.Id}, Manufacturer: {car.Manufacturer}, Model: {car.Model}, Year: {car.Year}");
+            }
+        }
+
+        public async Task UpdateCar(int id)
+        {
+            var car = await _carRepository.GetByIdAsync(id);
+
+            if (car != null)
+            {
+                car.Year = 2024;
+                car.Model = "Updated Model";
+
+                await _carRepository.UpdateAsync(car);
+            }
+        }
+
+        public async Task DeleteCar(int id)
+        {
+            await _carRepository.DeleteAsync(id);
+        }
+
+    }
+
+}
