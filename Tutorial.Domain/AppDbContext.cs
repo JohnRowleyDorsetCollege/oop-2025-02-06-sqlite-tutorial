6da02cc46bee6338315015ba830c1369b9750a53
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-06 09:43:02 +0000

    updated app context

---
+++ Tutorial.Domain/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Domain
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public AppDbContext()
+        {
+            
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+
+                options.Equals("Data Source=c:/sqlite/students-2025-02-06.db");
+
+            }
+
+        }
+    }
+}
