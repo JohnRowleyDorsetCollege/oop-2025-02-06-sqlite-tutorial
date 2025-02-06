0b3974fe0a4f411c5b7bd93210bd2813c658a69b
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-06 09:47:01 +0000

    Added program setup for sql lite

--- Tutorial.Console/Program.cs
+++ Tutorial.Console/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
 using Tutorial.Domain;
 
 Console.WriteLine("SQL Lite Tutorial");
 
 SQLitePCL.Batteries_V2.Init();
+
+var options = new DbContextOptionsBuilder<AppDbContext>()
+           .UseSqlite("Data Source=c:/sqlite/students20250205a.db")
+           .Options;
 
 var student1 = new Student() { Id = 1, Name = "John", Age = 18 };
 var student2 = new Student() { Id = 1, Name = "John", Age = 18 };
